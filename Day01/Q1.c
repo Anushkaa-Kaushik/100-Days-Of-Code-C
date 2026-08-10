4e272ed3d3d094524700a3020e3a0c6f9a5fb0e2
Anushkaa-Kaushik <anushka.37901@stu.upes.ac.in>
2026-08-10 15:10:28 +0530

    Implement sum of two numbers in Q1.c
    Fix the sum calculation to use the correct operator.

---
+++ Day01/Q1.c
@@ -0,0 +1,11 @@
+/*Write a program to input two numbers and display their sum.*/
+#include <stdio.h>
+
+int main()
+{
+    int a = 3;
+    int b = 4;
+   int sum = a+b; 
+    printf("%d", sum);
+    return 0;
+}
