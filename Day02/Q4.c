64d38a001bf94f8cb3ad31b1285cbd4faaa3b22b
Anushkaa-Kaushik <anushka.37901@stu.upes.ac.in>
2026-08-11 10:38:46 +0530

    Add program to calculate circle area and circumference
    Fix the format specifier for radius input and output.

---
+++ Day02/Q4.c
@@ -0,0 +1,10 @@
+/*Q4Write a program to calculate the area and circumference of a circle given its radius.*/
+#include <stdio.h>
+int main()
+{
+    int radius;
+    scanf("%f",&radius);
+    printf("area = %f,",3.14*radius*radius);
+    printf("perimeter = %f",3.14*2*radius);
+    
+}
