fb022e3e05695c2dd2254eda05b71c0b436c1306
Anushkaa-Kaushik <anushka.37901@stu.upes.ac.in>
2026-08-13 20:01:00 +0530

    Create Q7.c

---
+++ Day4/Q7.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+/*Q8: Write a program to find and display the sum of the first n natural numbers.
+
+
+Sample Test Cases:
+Input 1:
+5
+Output 1:
+Sum=15
+
+Input 2:
+10
+Output 2:
+Sum=55
+
+*/
+int n;
+printf("Enter n = ");
+scanf("%d",&n);
+int sum = n*(n+1)/2;
+printf("total sum = %d",sum);
+
+    return 0;
+}
