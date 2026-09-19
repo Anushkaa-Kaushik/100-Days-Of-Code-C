af26a840810de3a7eab7e57a401e2953ea13e5af
Anushkaa-Kaushik <315283784+Anushkaa-Kaushik@users.noreply.github.com>
2026-09-19 20:07:42 +0530

    Add diagonal sum calculation for square matrix
    Implement a program to calculate the sum of main diagonal elements of a square matrix.

---
+++ Day39/Q78.c
@@ -0,0 +1,35 @@
+/*Q78: Find the sum of main diagonal elements for a square matrix.
+Sample Test Cases:
+Input 1:
+3 3
+1 2 3
+4 5 6
+7 8 9
+Output 1:
+15
+
+*/
+#include <stdio.h>
+
+int main()
+{
+   int n,sum=0;
+   printf("enter n : ");
+   scanf("%d",&n);
+   int arr[n][n];
+   for(int i=0;i<n;i++){
+       for(int j=0;j<n;j++){
+      printf("enter arr[i][j] : ");
+      scanf("%d",&arr[i][j]);
+       }
+   }  for(int i=0;i<n;i++){
+       for(int j=0;j<n;j++){
+           if(i==j)
+           sum= sum+arr[i][j];
+       }
+   }
+   printf("%d",sum);
+        
+   
+    return 0;
+}
